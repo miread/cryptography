faf647ba60fad3ba6e701a09490dfbc614a2aeca
miread <miranda.evalyn@gmail.com>
2019-04-01 17:28:08 -0500

    Fixed comments

--- rail_fence_cipher.py
+++ rail_fence_cipher.py
@@ -1,9 +1,8 @@
 def encrypt(contents, key):
-
     result = ""
-
     for n in range(1, key + 1):
 
+        # First rail follows a simple distance formula
         if n == 1:
             distance = 2 * key - 3
             count = 0
@@ -15,6 +14,7 @@ def encrypt(contents, key):
                 else:
                     count += 1
 
+        # Middle rails are written twice per "cycle", alternating distances
         if n > 1 and n < key:
             distance_down = (2 * key) - (2 * n) - 1
             distance_up = 2 * n - 3
@@ -33,6 +33,7 @@ def encrypt(contents, key):
                     path = "down"
                 count += 1
 
+        # Last rail is like the first, but offset
         if n == key:
             distance = 2 * key - 3
             count = 1 - key
@@ -48,47 +49,41 @@ def encrypt(contents, key):
 
 
 def decrypt(contents, key):
-    dict = {}  # Holds length of each rail, then the rail itself
+    dict = {}
     content_length = len(contents)
     cycle = 2 * key - 2
     num_of_cycles = int(content_length / cycle)
     remainder = content_length % cycle
 
-    # Stores the number of complete cycles for each rail
+    # Middle rails are twice as long
     for n in range(1, key + 1):
         if n == 1 or n == key:
             dict[n] = num_of_cycles
-            #print("Rail " + str(n) + " has " + str(dict[n]) + " complete cycles")
         if n > 1 and n < key:
             dict[n] = num_of_cycles * 2
-            #print("Rail " + str(n) + " has " + str(dict[n]) + " complete cycles")
 
-    # Adds partial cycle
+    # A partial cycle must be counted, too
     for i in range(1, remainder + 1):
         if i <= key:
             dict[i] += 1
-            #print("Rail " + str(i) + " has " + str(dict[i]) + " cycles total")
         if i > key:
             dict[2 * key - i] += 1
-            #print("Rail " + str(i) + " has " + str(dict[i]) + " cycles total")
 
-    # Replaces the rail length with the rail contents
+    # We will replace the count with the actual contents of each rail
     count = 0
     for n in range(1, key + 1):
         dict[n], count = contents[count:count + dict[n]], count + dict[n]
-        #print("Rail " + str(n) + ": " + dict[n] + ", count: " + str(count))
 
+    # Reversed list form for efficient popping
     for n in range(1, key + 1):
         dict[n] = list(reversed(dict[n]))
-        #print("Rail " + str(n) + " reversed: ", dict[n])
 
+    # Pop the rails one-by-one, in order
     output = ""
     count = 1
     path = "down"
     for i in range(0, content_length):
-        #print(dict[count])
         output += dict[count].pop()
-        #print(output)
         if path == "down":
             if count == key:
                 path = "up"
@@ -103,6 +98,3 @@ def decrypt(contents, key):
                 count -= 1
 
     return output
-
-
-print(decrypt("WECRLTEERDSOEEFEAOCAIVDEN", 3))
